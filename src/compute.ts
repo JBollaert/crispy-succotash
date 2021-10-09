21b4b2ebc404155d3cd7bf3face0f9629b73a052
Jeffrey Bollaert <Jeffrey.bollaert@hotmail.com>
2021-10-09 11:04:47 +0200

    Solved

--- src/compute.ts
+++ src/compute.ts
@@ -1,5 +1,30 @@
 import { Game } from "./types";
 
 export function compute(game: Game): number {
-  throw new Error("Not yet implemented"); // TODO
+  let total: number = 0;
+
+  for (let i = 0; i < game.length; i++) {
+    let frame = game[i];
+    let nextFrame = game[i+1];
+    let thirdFrame = game[i+2];
+    
+    if(i === 9){//Last frame
+      total += frame[0]+frame[1]+frame[2];
+    }else{
+      if(frame[0] === 10){//Strike
+        if(nextFrame[0] === 10){//Double
+          if(i+2 <= 9 && thirdFrame[0] === 10){//Turkey +
+            total += 30;
+          }else{
+            total += i+2 <= 9 ? 20+thirdFrame[0] : 10+nextFrame[0]+nextFrame[1];
+          }
+        }else{
+          total += (10+nextFrame[0]+nextFrame[1])
+        }
+      }else{//Spare or no spare
+        total += frame[0]+frame[1] === 10 ? 10+nextFrame[0] : frame[0]+frame[1];
+      }
+    }
+  }
+  return total;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
-import http from "http";
 import express from "express";
 import { compute } from "./compute";
-
 export const app = express();
 
 app.use(express.json());
@@ -9,8 +7,15 @@ app.use(express.json());
 app.post("/compute", (request, response) => {
   const game = request.body.game;
   // TODO: Validate input
+  if(game.length !== 10 ){
+    response.sendStatus(400);
+  }
+  game.flat().forEach((e: number) => {
+        if(e < 0 || e > 10 || typeof e !== "number")  
+          response.sendStatus(400);
+  }); 
 
   const score = compute(game);
-
   // TODO: Return response
+  response.status(200).send({score: score});
 });
